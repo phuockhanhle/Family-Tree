59d821ea7ec9cf7dca94f1aefc42f8ada1939107
TrungDinhT <dinhthanhtrung1996@gmail.com>
2021-05-14 11:54:08 +0200

    WIP tests

---
+++ test/database_testRelation.go
@@ -0,0 +1,35 @@
+package test
+
+type TestCreateRelation struct {
+	info : string
+	input: Relation,
+	refRelation: string
+}
+
+var createRelationTestSuite = []TestCreateRelation {
+	TestCreateRelation{
+		info : "insert childOf with a person not having a spouse",
+		input: ...,
+		refRelation: ...,
+	},
+	TestCreateRelation{
+		info : "insert childOf with a person having a spouse",
+		input: ...,
+		refRelation: ...,
+	},
+	TestCreateRelation{
+		info : "insert spouse with a person not having a child",
+		input: ...,
+		refRelation: ...,
+	},
+	TestCreateRelation{
+		info : "insert spouse with a person having a child",
+		input: ...,
+		refRelation: ...,
+	},
+}
+
+func test(){
+	createRelation(input)
+	matchRelation(input.start, input.end) == role
+}
