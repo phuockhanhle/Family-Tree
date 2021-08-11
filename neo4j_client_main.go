c0d0a52a1c306cae6375d475df7f067bec893945
Phuoc-Khanh LE <phuockhanhnk94@gmail.com>
2021-08-11 11:01:44 +0200

    add people by file csv

--- neo4j_client_main.go
+++ neo4j_client_main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
+	"strconv"
 
 	model "github.com/phuockhanhle/familytree/model"
 )
@@ -11,25 +14,25 @@ func main() {
 	username := string("neo4j")
 	password := string("1234")
 
-	vis := model.Person{
-		ID:        model.GenerateID(),
-		IDTree:    model.GenerateID(),
-		FirstName: "Thanh Trung",
-		LastName:  "Dinh",
-		NickName:  "vis",
-		Gender:    model.Male,
-		HasChild:  false,
-	}
+	// vis := model.Person{
+	// 	ID:        model.GenerateID(),
+	// 	IDTree:    model.GenerateID(),
+	// 	FirstName: "Thanh Trung",
+	// 	LastName:  "Dinh",
+	// 	NickName:  "vis",
+	// 	Gender:    model.Male,
+	// 	HasChild:  false,
+	// }
 
-	ba_phuong := model.Person{
-		ID:        model.GenerateID(),
-		IDTree:    vis.IDTree,
-		FirstName: "Thanh Phuong",
-		LastName:  "Dinh",
-		NickName:  "nheo",
-		Gender:    model.Male,
-		HasChild:  true,
-	}
+	// ba_phuong := model.Person{
+	// 	ID:        model.GenerateID(),
+	// 	IDTree:    vis.IDTree,
+	// 	FirstName: "Thanh Phuong",
+	// 	LastName:  "Dinh",
+	// 	NickName:  "nheo",
+	// 	Gender:    model.Male,
+	// 	HasChild:  true,
+	// }
 
 	// vis.ID = "b34041b9-bda6-11eb-8c9d-b06ebfab25a7"
 
@@ -38,29 +41,54 @@ func main() {
 	driver.CreateInstance(uri, username, password)
 	defer driver.Close()
 
-	vis = driver.RunTransaction(model.InsertPerson, vis).(model.Person)
-	ba_phuong = driver.RunTransaction(model.InsertPerson, ba_phuong).(model.Person)
+	// vis = driver.RunTransaction(model.InsertPerson, vis).(model.Person)
+	// ba_phuong = driver.RunTransaction(model.InsertPerson, ba_phuong).(model.Person)
 
-	driver.RunTransaction(model.InsertRelation, model.Relation{
-		FromID:       vis.ID,
-		ToID:         ba_phuong.ID,
-		TypeRelation: "CHILDOF",
-	})
+	// driver.RunTransaction(model.InsertRelation, model.Relation{
+	// 	FromID:       vis.ID,
+	// 	ToID:         ba_phuong.ID,
+	// 	TypeRelation: "CHILDOF",
+	// })
 
-	p := driver.RunTransaction(model.MatchPersonByID, vis.ID).(model.Person)
-	fmt.Println(model.StructToMap(p))
+	// p := driver.RunTransaction(model.MatchPersonByID, vis.ID).(model.Person)
+	// fmt.Println(model.StructToMap(p))
 
-	r := model.Relation{
-		FromID:       vis.ID,
-		TypeRelation: "CHILDOF",
-	}
-	list_p := driver.RunTransaction(model.MatchPersonByRelation, r).([]model.Person)
-	fmt.Println(list_p)
-
-	// if err == nil {
-	// 	fmt.Println(greeting)
-	// } else {
-	// 	fmt.Println("failed!")
-	// 	fmt.Println(err)
+	// r := model.Relation{
+	// 	FromID:       vis.ID,
+	// 	TypeRelation: "CHILDOF",
 	// }
+	// list_p := driver.RunTransaction(model.MatchPersonByRelation, r).([]model.Person)
+	// fmt.Println(list_p)
+
+	csvFile, err := os.Open("./data/people.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, line := range csvLines {
+
+		gender := model.Male
+
+		if line[3] == "F" {
+			gender = model.Female
+		}
+
+		hasChild, _ := strconv.ParseBool(line[4])
+
+		tmp := model.Person{
+			FirstName: line[0],
+			LastName:  line[1],
+			NickName:  line[2],
+			Gender:    gender,
+			HasChild:  hasChild,
+		}
+
+		tmp = driver.RunTransaction(model.InsertPerson, tmp).(model.Person)
+	}
 }
