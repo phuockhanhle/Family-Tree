4651e7bc5db2fa921f59a647ee9c7288e7f111d9
Steve <snguyen@fff.fr>
2022-05-17 16:37:00 +0200

    test svg

--- src/components/pages/dashboard/DashBoard.tsx
+++ src/components/pages/dashboard/DashBoard.tsx
@@ -1,29 +1,35 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
-import { logOut } from "../../utils/HelperFuncs";
-import { familyList } from "../../utils/JsonExample";
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { useState } from "react";
+
+const width = '100vw';
+const height = 300;
+const circleX = '50vw';
+const circleY = height / 2;
+const circleRadius = 50;
+const initialMousePosition = {
+    x: circleX,
+    y: height / 2
+}
+
 
 export const DashBoard = () => {
-    const rows: GridRowsProp = [
-        { id: 1, col1: 'Hello', col2: 'World' },
-        { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-        { id: 3, col1: 'MUI', col2: 'is Amazing' },
-    ];
-
-    const columns: GridColDef[] = [
-        { field: 'col1', headerName: 'Column 1', width: 150 },
-        { field: 'col2', headerName: 'Column 2', width: 150 },
-    ];
+    const [mousePosition, setMousePosition] = useState(initialMousePosition);
+    const handleMouseMove = (event: any) => {
+        const { clientX, clientY } = event;
+        console.log({ clientX, clientY });
+        setMousePosition({ x: clientX, y: clientY });
+    }
 
     return (
-        <div style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
-            <svg width="100vw" height="500" >
-                <circle cx="50vw" cy="250" r="50" fill="yellow" stroke="black" stroke-width="2"></circle>
+        <>
+            <svg width={width} height={height} onMouseMove={handleMouseMove}>
+                <g>
+                    <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} fill="yellow" stroke="black" stroke-width="2"></circle>
+                </g>
             </svg>
             <Button variant="contained">Hi</Button>
-        </div>
+        </>
+
+
     );
 }
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,10 +1,3 @@
-import axios from "axios";
-import Cookies from "js-cookie";
-import { useEffect } from "react";
-import { isIE } from "react-device-detect";
-import { Navigate, useNavigate } from "react-router-dom";
-import { back_end_url, cookie_login_name, isLocal } from "../../../app/constants";
-import { cookieCheck, logOut } from "../../utils/helperFunc";
 import { Login } from "./Login";
 
 export const LoginPage = () => {
