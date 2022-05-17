1d049f7675814ee69d82e7276b082ceb5a11f4ae
Steve <snguyen@fff.fr>
2022-05-17 18:22:36 +0200

    using d3 csv async await

--- src/components/pages/dashboard/DashBoard.tsx
+++ src/components/pages/dashboard/DashBoard.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
-import { useState } from "react";
+import * as d3 from "d3";
+import { useCallback, useEffect, useState } from "react";
 
 const width = '100vw';
 const height = 300;
@@ -14,20 +15,38 @@ const initialMousePosition = {
 
 export const DashBoard = () => {
     const [mousePosition, setMousePosition] = useState(initialMousePosition);
-    const handleMouseMove = (event: any) => {
+    const [message, setMessage] = useState('')
+
+    // wont create a new version of 'handleMouseMove' if setMousePosition doesnt change
+    const handleMouseMove = useCallback((event: any) => {
         const { clientX, clientY } = event;
-        console.log({ clientX, clientY });
         setMousePosition({ x: clientX, y: clientY });
-    }
+    }, [setMousePosition])
+
+    // 
+
+    const csvUrl = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv'
+
+    useEffect(() => {
+        d3.csv(csvUrl).then(data => {
+            let message = '';
+            message = message + Math.round(d3.csvFormat(data).length / 1024) + ' kB\n';
+            message = message + data.length + ' rows\n';
+            message = message + data.columns.length + ' columns';
+            setMessage(message);
+            console.log(data);
+        }) 
+    }, [message])
+    
 
     return (
         <>
             <svg width={width} height={height} onMouseMove={handleMouseMove}>
                 <g>
-                    <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} fill="yellow" stroke="black" stroke-width="2"></circle>
+                    <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} fill="yellow" stroke="black" strokeWidth="2"></circle>
                 </g>
             </svg>
-            <Button variant="contained">Hi</Button>
+            <Button variant="contained">{message}</Button>
         </>
 
 
