8765b7e44a16cdc4833f7017f1db2eed8beb902a
lephuock <phuockhanhnk94@gmail.com>
2022-05-26 10:16:31 +0200

    refactor

--- neo4j_client_main.go
+++ neo4j_client_main.go
@@ -8,64 +8,14 @@ import (
 	"strconv"
 )
 
-
-func main() {
-	uri := string("bolt://localhost:7687")
-	username := string("neo4j")
-	password := string("1234")
-
-	var driver model.Neo4jDriver
-
-	driver.CreateInstance(uri, username, password)
-	defer driver.Close()
-
-	driver.DeleteDatabase()
-
-	vis := model.Person{
-		ID:        model.GenerateID(),
-		IDTree:    model.GenerateID(),
-		FirstName: "Thanh Trung",
-		LastName:  "Dinh",
-		NickName:  "vis",
-		Gender:    model.Male,
-		HasChild:  false,
-	}
-
-	ba_phuong := model.Person{
-		ID:        model.GenerateID(),
-		IDTree:    vis.IDTree,
-		FirstName: "Thanh Phuong",
-		LastName:  "Dinh",
-		NickName:  "nheo",
-		Gender:    model.Male,
-		HasChild:  true,
-	}
-
-	vis = driver.RunTransaction(model.InsertPerson, vis).(model.Person)
-	ba_phuong = driver.RunTransaction(model.InsertPerson, ba_phuong).(model.Person)
-
-	r := driver.RunTransaction(model.InsertRelation, model.Relation{
-		FromID:       vis.ID,
-		ToID:         ba_phuong.ID,
-		TypeRelation: "CHILDOF",
-	})
-
-	fmt.Print(r)
-
-	info := model.InfoUpdate{
-		PersonID: vis.ID,
-		Field:    "HasChild",
-		Value:    true,
-	}
-
-	driver.RunTransaction(model.UpdatePerson, info)
-
+func addPersonByCsv(driver model.Neo4jDriver) map[string]model.Person {
 	csvFile, err := os.Open("./data/people.csv")
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("Successfully Opened CSV file")
 	defer csvFile.Close()
+	var result = make(map[string]model.Person)
 
 	csvLines, err := csv.NewReader(csvFile).ReadAll()
 	if err != nil {
@@ -82,6 +32,8 @@ func main() {
 		hasChild, _ := strconv.ParseBool(line[4])
 
 		tmp := model.Person{
+			ID:        model.GenerateID(),
+			IDTree:    line[5],
 			FirstName: line[0],
 			LastName:  line[1],
 			NickName:  line[2],
@@ -89,8 +41,63 @@ func main() {
 			HasChild:  hasChild,
 		}
 
-		tmp = driver.RunTransaction(model.InsertPerson, tmp).(model.Person)
+		person := driver.RunTransaction(model.InsertPerson, tmp).(model.Person)
+		result[person.FirstName] = person
 	}
+	return result
+}
+
+func addRelationByCsv(driver model.Neo4jDriver, mapPersonByID map[string]model.Person) {
+	csvFile, err := os.Open("./data/relation.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	
+
+	for _, line := range csvLines {
+
+		fromPerson := mapPersonByID[line[0]]
+		toPerson := mapPersonByID[line[1]]
+
+		_ = driver.RunTransaction(model.InsertRelation, model.Relation{
+			FromID:       fromPerson.ID,
+			ToID:         toPerson.ID,
+			TypeRelation: line[2],
+		})
+
+		// if toPerson.Gender == model.Male {
+		// 	_ = driver.RunTransaction(model.UpdatePerson, model.InfoUpdate{
+		// 		PersonID: 	toPerson.ID,
+		// 		Field: 		"IDTree",
+		// 		Value: 		fromPerson.IDTree,
+		// 	})
+		// }		
+	}
+}
+
+
+func main() {
+	uri := string("bolt://localhost:7687")
+	username := string("neo4j")
+	password := string("1234")
+
+	var driver model.Neo4jDriver
+
+	driver.CreateInstance(uri, username, password)
+	defer driver.Close()
+
+	driver.DeleteDatabase()
+
+	var mapPersonByID = addPersonByCsv(driver)
+	addRelationByCsv(driver, mapPersonByID)
 
 	// var controller model.Controller
 
